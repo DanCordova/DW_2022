1922f6bd27eed7fbfdcc3b0b7717ae6de946f826
DanCordova <a01377242@itesm.mx>
2022-08-22 17:29:47 -0500

    Clase 5 JavaScript

---
+++ js/intro.js
@@ -0,0 +1,14 @@
+
+
+
+let comer_variado  = (comida) => {
+    console.log("Comiendo"+comida)
+};
+
+comer_variado("torta");
+
+const arreglo = [1,2,3,"Elemento"];
+arreglo.push("ISDR");
+console.log(arreglo);
+
+document.write(arreglo);
